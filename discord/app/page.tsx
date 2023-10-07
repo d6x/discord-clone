8617b3b9e2bec1896a9e5d14fe8f87bdf9f2769d
Liam Molloy <liam@Liams-MacBook-Air.local>
2023-10-07 15:32:11 +0100

    feat: Added initial files and environment setup

---
+++ discord/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <div className="">
+      <p className="text-3xl"> 
+        test 
+      </p>
+      <Button variant="secondary"> 
+        Login
+      </Button>
+    </div>
+  )
+}
